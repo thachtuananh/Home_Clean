67fcee15bc3bd23cd4d53f630723297d6a8f7bd4
Anh Thach <anhtthe161427@fpt.edu.vn>
2025-01-17 10:19:45 +0700

    Initial commit

---
+++ src/main/java/com/example/api_homeclean/ApiHomeCleanApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_homeclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHomeCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiHomeCleanApplication.class, args);
+    }
+
+}
